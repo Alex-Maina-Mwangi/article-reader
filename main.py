48b5728ff374b71c0b3e29ea602ea22ba16ac3f4
Alex Maina <108124585+Alex-Maina-Mwangi@users.noreply.github.com>
2025-04-25 00:27:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -58,16 +58,8 @@ for i in pmcids:
             # Extract mentioned country names
             mentioned_countries = [country for country in country_names if country in paragraph]
 
-            kenyan_counties = [ "Baringo", "Bomet", "Bungoma", "Busia", "Elgeyo-Marakwet", "Embu", 
-                        "Garissa", "Homa Bay", "Isiolo", "Kajiado", "Kakamega", "Kericho", 
-            "Kiambu", "Kilifi", "Kirinyaga", "Kisii", "Kisumu", "Kitui", "Kwale", 
-            "Laikipia", "Lamu", "Machakos", "Makueni", "Mandera", "Marsabit", 
-            "Meru", "Migori", "Mombasa", "Murang'a", "Nairobi", "Nakuru", 
-            "Nandi", "Narok", "Nyamira", "Nyandarua", "Nyeri", "Samburu", 
-            "Siaya", "Taita-Taveta", "Tana River", "Tharaka-Nithi", "Trans Nzoia", 
-            "Turkana", "Uasin Gishu", "Vihiga", "Wajir", "West Pokot"
-                ]
-
+            kenyan_counties = [ ]
+       
             # Load spaCy's English language model
             nlp = spacy.load("en_core_web_sm")
 
